28606f0db4d3cd7ad6849e2720e3ed1b9a42dbac
wujiulin <wu.jiulin@whaley.cn>
2018-01-18 16:07:56 +0800

    权限相关功能点

---
+++ illidan-server/src/main/java/cn/whaley/datawarehouse/illidan/server/service/AuthService.java
@@ -0,0 +1,55 @@
+package cn.whaley.datawarehouse.illidan.server.service;
+
+import cn.whaley.datawarehouse.illidan.common.domain.project.Project;
+import cn.whaley.datawarehouse.illidan.common.domain.table.TableInfo;
+import cn.whaley.datawarehouse.illidan.common.util.ConfigUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public class AuthService {
+
+    @Autowired
+    private AuthorizeHttpService authorizeHttpService;
+    private String projectNodeId = ConfigUtils.get("newillidan.authorize.project_node_id");
+    private String tableNodeId = ConfigUtils.get("newillidan.authorize.table_node_id");
+    private String sysId = ConfigUtils.get("newillidan.authorize.sys_id");
+
+    public String createProjectAuth(Project project, String createUserName){
+        //project_112
+        //read_project_112
+        //write_project_112
+        //publish_project_112
+        String dirName = "project_"+project.getId();
+        return authorizeHttpService.createAuth(projectNodeId, dirName, createUserName);
+    }
+
+    public String createTableAuth(TableInfo tableInfo, String createUserName){
+        return "";
+    }
+
+    public List<Project> filterProjectList(List<Project> projects, String userName){
+        return null;
+    }
+
+    public List<TableInfo> filterTableList(List<TableInfo> tableInfos, String userName){
+        return null;
+    }
+
+    public boolean hasTablePermission(Long tabletId, String operateType, String userName) {
+        return true;
+    }
+
+    public boolean hasProjectPermission(Long projectId, String operateType, String userName) {
+        return true;
+    }
+
+    public boolean hasTaskGroupPermission(Long taskGroupId, String operateType, String userName) {
+        return true;
+    }
+
+    public boolean hasTaskPermission(Long taskId, String operateType, String userName) {
+        return true;
+    }
+
+}
